b6fe92463b48ad400b413f9ad60ac0485b87ab1d
Patrick Nymark <patricknymark@Patricks-iMac.local>
2020-02-06 12:49:00 +0100

    initial commit

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const errorHandler = require('./helpers/error-handler');
+
+// initialize
+const app = express();
+
+// env config
+require('dotenv').config();
+
+// logger
+app.use(logger('dev'));
+
+console.log(process.env.MONGO_URI)
+
+// mongodb config
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(res => console.log('Database connected'))
+  .catch(err => console.log(err));
+
+// body-parser middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// routes
+app.use('/api/auth', require('./controllers/auth.controller'));
+
+// global error handler
+app.use(errorHandler);
+
+const port = 5000 || process.env.PORT;
+
+app.listen(port, () => console.log(`Server running on ${port}`));
