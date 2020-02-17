54f468fd7b2acb977ed1f0f3dff1ca48691722e9
Patrick Nymark <patricknymark@Patricks-iMac.local>
2020-02-17 14:46:19 +0100

    fix

--- client/src/main/Landing.js
+++ client/src/main/Landing.js
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 import './sass/homepage.scss'
 import Menu from '../common/Menu'
 import Logo from '../images/logo-light.png'
+import WorldGold from '../images/world-gold-no-bg.png';
+import ScandiGold from '../images/scandi-gold-no-bg.png';
+
 
 class HomePage extends PureComponent {
     constructor(props) {
@@ -30,7 +33,19 @@ class HomePage extends PureComponent {
                         <Menu light={true}/>
                     </div>
                 </div>
-
+                <div className="home-headline">
+                    <div>
+                        <h1>SUBLIME OR BUST</h1>
+                    </div>
+                </div>
+                <div className="home-awards">
+                    <div className="home-award">
+                        <img src={WorldGold} />
+                    </div>
+                    <div className="home-award">
+                        <img src={ScandiGold} />
+                    </div>
+                </div>
                 <div className="home-content">
                     <p>Content</p>
                 </div>
