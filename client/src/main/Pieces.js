ea75b75e70e3006a4354e7056f9915b696e76e59
Patrick Nymark <patricknymark@Patricks-iMac.local>
2020-02-18 00:57:57 +0100

    finished pieces v1

--- client/src/main/Pieces.js
+++ client/src/main/Pieces.js
@@ -3,6 +3,9 @@ import { Helmet } from "react-helmet";
 import axios from 'axios';
 import Header from '../common/Header';
 import Muscovado from '../images/muscovado-caramel.jpg'
+import Bergamot from '../images/bergamot.jpg'
+import Iceland from '../images/inland_ice.jpg'
+import Island from '../images/bornholm3.jpg'
 import './sass/pieces.scss'
 
 class Pieces extends PureComponent {
@@ -54,9 +57,30 @@ class Pieces extends PureComponent {
                             </div>
                         </div>
                         <div className="pieces">
-                            <div className="piece-logo"><img src={Muscovado}></img></div>
+                            <div className="piece-logo"><img src={Bergamot}></img></div>
                             <div className="piece-content">
-                                <h3>Muscovado Caramel</h3>
+                                <h3>Olive Oil Bergamot</h3>
+                                <p>Quisque id lacus nisl. Mauris et posuere erat. Aenean et sodales elit. Vivamus eget libero ornare, maximus lacus sit amet, cursus neque. Phasellus vel urna eget diam bibendum dignissim sed quis ante. Fusce maximus libero vel mi imperdiet, ornare convallis eros ornare. Pellentesque molestie risus at commodo volutpat. Proin pellentesque risus et leo mollis volutpat.</p>
+                            </div>
+                        </div>
+                        <div className="pieces">
+                            <div className="piece-logo"><img style={{ position: 'relative', top: '-50px'}} src={Iceland}></img></div>
+                            <div className="piece-content">
+                                <h3>Inland Ice</h3>
+                                <p>Quisque id lacus nisl. Mauris et posuere erat. Aenean et sodales elit. Vivamus eget libero ornare, maximus lacus sit amet, cursus neque. Phasellus vel urna eget diam bibendum dignissim sed quis ante. Fusce maximus libero vel mi imperdiet, ornare convallis eros ornare. Pellentesque molestie risus at commodo volutpat. Proin pellentesque risus et leo mollis volutpat.</p>
+                            </div>
+                        </div>
+                        <div className="pieces">
+                            <div className="piece-logo"><img src={Bergamot}></img></div>
+                            <div className="piece-content">
+                                <h3>Olive Oil Bergamot</h3>
+                                <p>Quisque id lacus nisl. Mauris et posuere erat. Aenean et sodales elit. Vivamus eget libero ornare, maximus lacus sit amet, cursus neque. Phasellus vel urna eget diam bibendum dignissim sed quis ante. Fusce maximus libero vel mi imperdiet, ornare convallis eros ornare. Pellentesque molestie risus at commodo volutpat. Proin pellentesque risus et leo mollis volutpat.</p>
+                            </div>
+                        </div>
+                        <div className="pieces">
+                            <div className="piece-logo"><img src={Island}></img></div>
+                            <div className="piece-content">
+                                <h3>Island Water</h3>
                                 <p>Quisque id lacus nisl. Mauris et posuere erat. Aenean et sodales elit. Vivamus eget libero ornare, maximus lacus sit amet, cursus neque. Phasellus vel urna eget diam bibendum dignissim sed quis ante. Fusce maximus libero vel mi imperdiet, ornare convallis eros ornare. Pellentesque molestie risus at commodo volutpat. Proin pellentesque risus et leo mollis volutpat.</p>
                             </div>
                         </div>
